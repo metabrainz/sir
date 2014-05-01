672e01272f5045c66920f0a508a8ef9a99e1c425
Wieland Hoffmann <themineo@gmail.com>
2014-05-01 12:28:19 +0200

    add some debugging statements to query_result_to_dict

--- sir/indexing.py
+++ sir/indexing.py
@@ -29,14 +29,18 @@ def query_result_to_dict(entity, obj):
     """
     data = {}
     for field in entity.fields:
+        logger.debug("Field: %s", field.name)
         fieldname = field.name
         tempvals = set()
         for path in field.paths:
+            logger.debug("Path: %s", path)
             for val in querying._iterate_path_values(path, obj):
                 tempvals.add(val)
+        logger.debug("Values: %s", tempvals)
         if len(tempvals) == 1:
             tempvals = tempvals.pop()
         if field.transformfunc is not None:
             tempvals = field.transformfunc(tempvals)
+            logger.debug("Values after applying transformfunc: %s", tempvals)
         data[fieldname] = tempvals
     return data
