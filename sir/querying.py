2913302790ac88f7f20012f37738b2dddb322d51
Wieland Hoffmann <themineo@gmail.com>
2014-05-14 17:17:01 +0200

    Only load columns that contain required values

--- sir/querying.py
+++ sir/querying.py
@@ -3,8 +3,9 @@
 import logging
 
 
+from collections import defaultdict
 from sqlalchemy import func
-from sqlalchemy.orm import Load
+from sqlalchemy.orm import class_mapper, Load
 from sqlalchemy.orm.interfaces import ONETOMANY, MANYTOONE
 from sqlalchemy.orm.properties import RelationshipProperty
 from sqlalchemy.orm.query import Query
@@ -12,35 +13,100 @@ from sqlalchemy.orm.query import Query
 logger = logging.getLogger("sir")
 
 
-def build_entity_query(entity):
-    """Builds a :class:`sqla:sqlalchemy.orm.query.Query` object for ``entity``
-       (an instance of :class:`sir.schema.searchentities.SearchEntity`) that eagerly loads the
-       values of all search fields.
-
-       :param sir.schema.searchentities.SearchEntity entity:
-       :rtype: :class:`sqla:sqlalchemy.orm.query.Query`
+def merge_paths(field_paths):
     """
-    model = entity.model
-    q = Query(model)
-    for field in entity.fields:
-        # Walk `field.path` and apply loading strategies to each element
-        for path in field.paths:
-            model = entity.model
+    Given a list of paths as ``field_paths``, return a dict that, for each
+    level of the path, includes a dictionary whose keys are the columns to load
+    and the values are other dictionaries of the described structure.
+
+
+    :param field_paths:
+    :type field_paths: [str]
+    :rtype: dict
+    """
+    paths = defaultdict(set)
+    for path_list in field_paths:
+        for path in path_list:
+            path_length = path.count(".")
+            current_path_dict = paths
+            for i, pathelem in enumerate(path.split(".")):
+                # Build a new dict for the next level, if there is one
+                if i >= path_length:
+                    # If not, still add the current element as a key so its
+                    # column gets loaded
+                    current_path_dict[pathelem] = ""
+                    break
+
+                if pathelem in current_path_dict:
+                    new_path_dict = current_path_dict[pathelem]
+                else:
+                    new_path_dict = defaultdict(set)
+                current_path_dict[pathelem] = new_path_dict
+                current_path_dict = new_path_dict
+    return paths
+
+
+def _defer_everything_but(mapper, load, *columns):
+    for prop in mapper.iterate_properties:
+        if hasattr(prop, "columns"):
+            key = prop.key
+            if key not in columns and key[:-3] not in columns and \
+               key[-3:] != "_id" and key != "position":
+               # We need the _id columns for subqueries and joins
+               # Position is needed because sqla automatically orders by
+               # artist_credit_name.position
+                logger.debug("Deferring %s on %s", key, mapper)
+                load.defer(key)
+    return load
+
+
+def build_entity_query(entity):
+    """
+    Builds a :class:`sqla:sqlalchemy.orm.query.Query` object for ``entity``
+    (an instance of :class:`sir.schema.searchentities.SearchEntity`) that
+    eagerly loads the values of all search fields.
+
+   :param sir.schema.searchentities.SearchEntity entity:
+   :rtype: :class:`sqla:sqlalchemy.orm.query.Query`
+    """
+    root_model = entity.model
+    query = Query(root_model)
+    paths = [field.paths for field in entity.fields]
+    merged_paths = merge_paths(paths)
+
+    for field_paths in paths:
+        for path in field_paths:
+            current_merged_path = merged_paths
+            model = root_model
             load = Load(model)
-            for pathelem in path.split('.'):
-                prop = getattr(model, pathelem).property
+            split_path = path.split(".")
+            for pathelem in split_path:
+                current_merged_path = current_merged_path[pathelem]
+                column = getattr(model, pathelem)
+                prop = column.property
+                
                 if isinstance(prop, RelationshipProperty):
+                    pk = column.mapper.primary_key[0].name
                     if prop.direction == ONETOMANY:
                         load = load.subqueryload(pathelem)
                     elif prop.direction == MANYTOONE:
                         load = load.joinedload(pathelem)
                     else:
                         load = load.defaultload(pathelem)
+                    required_columns = current_merged_path.keys()
+                    required_columns.append(pk)
                     # Get the mapper class of the current element of the path so
                     # the next iteration can access it.
                     model = prop.mapper.class_
-            q = q.options(load)
-    return q
+
+                    logger.debug("Loading only %s on %s",
+                                 required_columns,
+                                 model)
+                    load = _defer_everything_but(class_mapper(model),
+                                                 load,
+                                                 *required_columns)
+            query = query.options(load)
+    return query
 
 
 def _iterate_path_values(path, obj):
