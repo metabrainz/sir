efca5f9ca164030d12d440082033101f9393fba9
Jeff Weeks <jeff@jeffweeks.io>
2015-07-08 17:07:51 -0400

    added releasegroup type, mapped name to allow for releasegroupaccent

--- sir/schema/__init__.py
+++ sir/schema/__init__.py
@@ -288,7 +288,7 @@ SearchRelease = E(models.Release, [
 
 SearchReleaseGroup = E(modelext.CustomReleaseGroup, [
     F("mbid", "gid"),
-    F("releasegroup", "name"),
+    F("name", "name"),
     F("arid", "artist_credit.artists.artist.gid"),
     F("artist", "artist_credit.name"),
     F("artistname", "artist_credit.artists.artist.name"),
@@ -299,6 +299,7 @@ SearchReleaseGroup = E(modelext.CustomReleaseGroup, [
     F("status", "releases.status.name"),
     F("comment", "comment"),
     F("tag", "tags.tag.name"),
+    F("type", "type"), // TODO path?
     F("primarytype", "type.name"),
     F("secondarytype", "secondary_types.secondary_type.name")
 ],
