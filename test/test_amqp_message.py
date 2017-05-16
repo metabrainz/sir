7c7643b7a1b6fbc26b76d9dfd9dbe487fc52b98c
Roman Tsukanov <roman@tsukanov.me>
2017-05-16 18:03:27 +0200

    Fix tests

--- test/test_amqp_message.py
+++ test/test_amqp_message.py
@@ -8,15 +8,16 @@ from sir.amqp.message import (InvalidMessageContentException,
 
 
 class AmqpMessageTest(unittest.TestCase):
+
     @staticmethod
-    def _parsed_message(body="artist 123 456", channel="search.index"):
+    def _parsed_message(body='{"_table": "artist", "id": "42"}', channel="search.index"):
         msg = Message(body=body)
         parsed_message = PMessage.from_amqp_message(channel, msg)
         return parsed_message
 
     def test_message_parses(self):
         parsed_message = self._parsed_message()
-        self.assertEqual(parsed_message.entity_type, "artist")
+        self.assertEqual(parsed_message.table_name, "artist")
         self.assertEqual(parsed_message.message_type, MESSAGE_TYPES.index)
 
     def test_invalid_channel_raises(self):
@@ -27,10 +28,4 @@ class AmqpMessageTest(unittest.TestCase):
         self.assertRaises(ValueError, self._parsed_message)
 
     def test_message_too_short_raises(self):
-        self.assertRaises(InvalidMessageContentException, self._parsed_message,
-                          body="foo")
-
-    def test_non_delete_converts_to_int(self):
-        parsed_message = self._parsed_message(channel="search.index")
-        map(lambda id_: self.assertTrue(isinstance(id_, int)),
-            parsed_message.ids)
+        self.assertRaises(InvalidMessageContentException, self._parsed_message, body="foo")
