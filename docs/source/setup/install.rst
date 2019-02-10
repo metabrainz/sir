564ecc5928b96ed6a8da51694cd8fbf56b7afd2a
yvanzo <yvanzo@metabrainz.org>
2019-02-10 09:35:47 +0100

    Expand environment variables in config values
    https://docs.python.org/2.7/library/os.path.html#os.path.expandvars

--- docs/source/setup/install.rst
+++ docs/source/setup/install.rst
@@ -36,6 +36,11 @@ The easiest way to run sir at the moment is to use a `virtual environment
     pip install -r requirements.txt
     cp config.ini.example config.ini
 
+Note: Environment variables can be used in `config.ini` with the syntaxes
+`$NAME` and `${NAME}`.  Undefined variables will not be replaced at all.
+Escaping is not supported.
+
+
 You can now use sir via::
 
     python -m sir
--- sir/config.py
+++ sir/config.py
@@ -3,9 +3,15 @@
 import ConfigParser
 import os.path
 
-#: A :class:`ConfigParser.SafeConfigParser` instance holding the configuration
+#: A :class:`SafeExpandingConfigParser` instance holding the configuration
 #: data.
-CFG = None  # type: ConfigParser.SafeConfigParser
+CFG = None  # type: SafeExpandingConfigParser
+
+
+class SafeExpandingConfigParser(ConfigParser.SafeConfigParser, object):
+    def _interpolate(self, section, option, rawval, vars):
+        return os.path.expandvars(super(SafeExpandingConfigParser,
+            self)._interpolate(section, option, rawval, vars))
 
 
 class ConfigError(Exception):
@@ -15,10 +21,10 @@ class ConfigError(Exception):
 def read_config():
     """
     Read config files from all possible locations and set
-    :const:`sir.config.CFG` to a :class:`ConfigParser.SafeConfigParser`
+    :const:`sir.config.CFG` to a :class:`SafeExpandingConfigParser`
     instance.
     """
-    config = ConfigParser.SafeConfigParser()
+    config = SafeExpandingConfigParser()
     read_files = config.read([os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         "..", "config.ini"
