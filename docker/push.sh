d44f601fb26b79b3a3157c123343baaf87cd859e
yvanzo <yvanzo@users.noreply.github.com>
2019-06-18 10:55:56 +0200

    Add labels and tag to docker image (#106)
    Tag docker image with:
    * git ref (most recent tag, or current commit, with dirty mark)
    * deployment target (currently 'git2consul' in production)
    
    Label docker image with:
    * sir version derived from git ref
    * metabrainz/python base image version
    * build timestamp

--- docker/push.sh
+++ docker/push.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+#
+# Build image from the currently checked out version of SIR
+# and push it to the Docker Hub, tagged with version.
+#
+# Usage:
+#   $ ./push.sh
+
+set -e -u
+
+vcs_ref=`git describe --always --broken --dirty`
+version=${vcs_ref#v}
+deployment=git2consul
+tag=${version}-${deployment}
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
-docker build -t metabrainz/sir .
-docker push metabrainz/sir
+docker build \
+  --build-arg SIR_VERSION=${version} \
+  --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+  --build-arg VCS_REF=${vcs_ref} \
+  --tag metabrainz/sir:${tag} .
+docker push metabrainz/sir:${tag}
