b73cb8f32e1aa8435f89f65088f123bd6ff7170f
Alastair Porter <alastair@porter.net.nz>
2019-08-06 10:44:27 +0200

    Stop reindexing right away on unexpected exception

--- sir/indexing.py
+++ sir/indexing.py
@@ -156,11 +156,11 @@ def _multiprocessed_import(entity_names, live=False, entities=None):
                 p.terminate()
                 p.join()
             pool.terminate()
-            pool.join()
             raise
         except Exception as exc:
             logger.error("Failed to import %s.", e)
             logger.exception(exc)
+            pool.terminate()
         else:
             logger.debug("Importing %s successful!", e)
         entity_data_queue.put(STOP)
