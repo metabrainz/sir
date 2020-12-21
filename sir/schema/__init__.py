9cc920a37a44ea138713f4855d5e381052775a69
yvanzo <yvanzo@metabrainz.org>
2020-12-21 12:09:29 +0100

    SEARCH-384 (indexer): RG’s first release date field
    Add `firstreleasedate` (advanced) search field to `release-group` core.
    
    It goes with PRs metabrainz/mbsssss#54 (and metabrainz/mb-solr#46).

--- sir/schema/__init__.py
+++ sir/schema/__init__.py
@@ -437,6 +437,8 @@ SearchReleaseGroup = E(modelext.CustomReleaseGroup, [
     F("artist", "artist_credit.name"),
     F("artistname", "artist_credit.artists.artist.name"),
     F("creditname", "artist_credit.artists.name"),
+    F("firstreleasedate", "first_release_date.first_release_date",
+      transformfunc=tfs.index_partialdate_to_string),
     F("release", "releases.name"),
     F("reid", "releases.gid"),
     F("releases", "release_count", transformfunc=tfs.integer_sum, trigger=False),
@@ -460,7 +462,6 @@ SearchReleaseGroup = E(modelext.CustomReleaseGroup, [
                 "artist_credit.artists.artist.gid",
                 "artist_credit.artists.artist.sort_name",
                 "artist_credit.artists.artist.comment",
-                "first_release_date.first_release_date",
                 "tags.count", "type.gid",
                 "releases.status.gid",
                 "secondary_types.secondary_type.gid"
