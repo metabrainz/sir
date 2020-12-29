38a78351e2a924a85138dfd3a453764f3150600b
yvanzo <yvanzo@metabrainz.org>
2020-12-29 16:21:10 +0100

    Log building docker image to a local file

--- docker/push.sh
+++ docker/push.sh
@@ -8,6 +8,8 @@
 
 set -e -u
 
+image_name='metabrainz/sir'
+
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 DOCKER_CMD=${DOCKER_CMD:-docker}
@@ -15,10 +17,12 @@ vcs_ref=`git describe --always --broken --dirty --tags`
 version=${vcs_ref#v}
 deployment=git2consul
 tag=${version}-${deployment}
+timestamp=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 
 ${DOCKER_CMD} build \
   --build-arg SIR_VERSION=${version} \
   --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
   --build-arg VCS_REF=${vcs_ref} \
-  --tag metabrainz/sir:${tag} .
-${DOCKER_CMD} push metabrainz/sir:${tag}
+  --tag ${image_name}:${tag} . \
+  | tee ./"build-${version}-at-${timestamp}.log"
+${DOCKER_CMD} push ${image_name}:${tag}
