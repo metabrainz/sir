dff0a113406c98f8c86ab1d437938bb4815dd170
yvanzo <yvanzo@metabrainz.org>
2022-05-31 13:02:37 +0100

    Improve test script
    Following the model of `docker/push.sh`:
    
    - Document expected behavior and usage with heading comments
    - Exit on error, for example if the first Docker Compose command failed
    - Support running from any working directory (using `cd`)
    - Support any Docker Compose setup and version:
      * Run `docker-compose` by default assuming v1 and `docker` group
      * Support `docker compose` for Docker Compose v2
      * Support `sudo ...` if needed

--- test.sh
+++ test.sh
@@ -1,6 +1,22 @@
-docker-compose -f docker/docker-compose.test.yml -p sir-test up -d musicbrainz_db
-docker-compose -f docker/docker-compose.test.yml -p sir-test build
-docker-compose -f docker/docker-compose.test.yml -p sir-test run test \
+#!/usr/bin/env bash
+#
+# Run tests and return 0 if these passed successfully.
+#
+# Usage:
+#     ./test.sh
+# Or:
+#      DOCKER_COMPOSE_CMD='sudo docker-compose' ./test.sh
+
+set -o errexit -o nounset
+
+cd "$(dirname "${BASH_SOURCE[0]}")/"
+
+DOCKER_COMPOSE_CMD=${DOCKER_COMPOSE_CMD:-docker-compose}
+
+$DOCKER_COMPOSE_CMD -f docker/docker-compose.test.yml -p sir-test up -d musicbrainz_db
+$DOCKER_COMPOSE_CMD -f docker/docker-compose.test.yml -p sir-test build
+set +o errexit
+$DOCKER_COMPOSE_CMD -f docker/docker-compose.test.yml -p sir-test run test \
     dockerize -wait tcp://musicbrainz_db:5432 -timeout 600s \
         bash -c "py.test --junitxml=/data/test_report.xml \
             --cov=sir \
@@ -8,5 +24,5 @@ docker-compose -f docker/docker-compose.test.yml -p sir-test run test \
             --cov-report html:/data/coverage-html \
             $*"
 RET=$?
-docker-compose -f docker/docker-compose.test.yml -p sir-test down
+$DOCKER_COMPOSE_CMD -f docker/docker-compose.test.yml -p sir-test down
 exit $RET
