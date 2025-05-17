96d127536126fa7f413969484de5b55f48e9d13f
yvanzo <yvanzo@metabrainz.org>
2025-05-17 19:29:56 +0200

    Set Docker/Compose commands for tests
    - Guess the command for Docker according to the platform
    - Allow to use alternatives such as Podman/Compose
    - Support using `sudo` if required (Linux-only)
    - Allow to customize the compose command
      (Do not preset the legacy standalone command `docker-compose` though)
    
    References:
    * https://github.com/metabrainz/musicbrainz-docker/blob/v-2025-05-05.0/admin/lib/common.inc.bash
    * https://docs.podman.io/en/latest/markdown/podman-compose.1.html
    * https://docs.docker.com/engine/install/linux-postinstall/
    * https://docs.docker.com/compose/install/

--- test.sh
+++ test.sh
@@ -5,13 +5,49 @@
 # Usage:
 #     ./test.sh
 # Or:
+#      DOCKER_CMD='sudo podman' ./test.sh
+# Or:
 #      DOCKER_COMPOSE_CMD='sudo docker compose' ./test.sh
 
 set -o errexit -o nounset
 
 cd "$(dirname "${BASH_SOURCE[0]}")/"
 
-DOCKER_COMPOSE_CMD=${DOCKER_COMPOSE_CMD:-docker compose}
+SCRIPT_NAME=$(basename "$0")
+
+# Set Docker/Compose commands
+
+if [ -z ${DOCKER_CMD:+smt} ]
+then
+case "$OSTYPE" in
+  darwin*) # Mac OS X
+    DOCKER_CMD='docker'
+    ;;
+  linux*)
+    if groups | grep -Eqw 'docker|root'
+    then
+      DOCKER_CMD='docker'
+    elif groups | grep -Eqw 'sudo|wheel'
+    then
+      DOCKER_CMD='sudo docker'
+    else
+      echo >&2 "$SCRIPT_NAME: cannot set docker command: please either"
+      echo >&2 "  * add the user '$USER' to the group 'docker' or 'sudo'"
+      echo >&2 "  * or set the variable \$DOCKER_CMD"
+      exit 77 # EX_NOPERM
+    fi
+    ;;
+  *)
+    echo >&2 "$SCRIPT_NAME: cannot detect platform to set docker command"
+    echo >&2 "Try setting the variable \$DOCKER_CMD appropriately"
+    exit 71 # EX_OSERR
+    ;;
+  esac
+fi
+
+DOCKER_COMPOSE_CMD=${DOCKER_COMPOSE_CMD:-${DOCKER} compose}
+
+# Run tests
 
 $DOCKER_COMPOSE_CMD -f docker/docker-compose.test.yml -p sir-test up -d musicbrainz_db
 $DOCKER_COMPOSE_CMD -f docker/docker-compose.test.yml -p sir-test build
