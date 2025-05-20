2ba1ffad7ea9db9c2ed5a9f0030a358d2817b904
Kartik Ohri <kartikohri13@gmail.com>
2025-05-21 02:03:53 +0530

    Eagerly load medium.gid for recordings
    `convert_medium_from_track` makes use of `convert_medium` which accesses
    the newly added `medium.gid` field. Eagerly load this field to avoid triggering
    a new query just to fetch this one field. This issue was caught by the `raiseload`
    feature added in the preceding commit.

--- sir/schema/__init__.py
+++ sir/schema/__init__.py
@@ -353,6 +353,7 @@ SearchRecording = E(modelext.CustomRecording, [
                 "tags.count",
                 "tags.tag.name",
                 "tracks.length",
+                "tracks.medium.gid",
                 "tracks.medium.cdtocs.id",
                 "tracks.medium.release.artist_credit.artists.artist.comment",
                 "tracks.medium.release.artist_credit.artists.artist.gid",
