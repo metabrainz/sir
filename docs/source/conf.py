21690896fc7323b19380ea6a808e155eba5549a3
yvanzo <yvanzo@metabrainz.org>
2025-05-21 18:52:48 +0200

    Fix docs warning: duplicate label rabbitmq
    Since the file `service/rabbitmq.rst` is included by `service/index.rst`
    then it should be excluded from the top-level.
    
    Reference: https://sphinx-themed.readthedocs.io/en/latest/usage/configuration.html#confval-exclude_patterns

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,7 +75,8 @@ release = __version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["setup/amqp.rst", "setup/install.rst"]
+exclude_patterns = ["setup/amqp.rst", "setup/install.rst",
+                    "service/rabbitmq.rst"]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
