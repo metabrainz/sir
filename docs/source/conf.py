386693e3c913e35a373802a73e75336b1d6f3921
Kartik Ohri <kartikohri13@gmail.com>
2025-05-22 02:09:49 +0530

    Use readthedocs theme for SIR docs
    readthedocs has started to obey the theme specified in the project's sphinx
    configuration since a while. The current sphinx theme configuration of sir docs
    predates RTD and was not actually not being used for as long as the docs have
    been on RTD. Update the documentation to explicitly use the RTD theme for docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,27 +103,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {
-        "footerbgcolor": "#e7e7e7",
-        "footertextcolor": "#444444",
-        "sidebarbgcolor": "#ffffff",
-        "sidebartextcolor": "#000000",
-        "sidebarlinkcolor": "002bba",
-        "relbarbgcolor": "#5c5789",
-        "relbartextcolor": "#000000",
-        "bgcolor": "#ffffff",
-        "textcolor": "#000000",
-        "linkcolor": "#002bba",
-        "headbgcolor": "#ffba58",
-        "headtextcolor": "#515151",
-        "codebgcolor": "#dddddd",
-        "codetextcolor": "#000000"
-        }
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
