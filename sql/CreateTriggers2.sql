274f5dc7daeaedf1ecd6da6373240d235a9b8c9c
Michael Wiencek <mwtuea@gmail.com>
2025-09-05 09:59:32 -0500

    Add SQL scripts for dbmirror2 setup
    The eventual goal here is to ingest changes from MB by copying the
    `dbmirror2.pending_data` table, rather than maintaining separate triggers that
    push changes through rabbitmq.

---
+++ sql/CreateTriggers2.sql
@@ -0,0 +1,5 @@
+CREATE TRIGGER copy_pending_data_sir
+    AFTER INSERT ON dbmirror2.pending_data
+    REFERENCING NEW TABLE AS inserted_rows
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION sir.a_ins_dbmirror2_pending_data();
