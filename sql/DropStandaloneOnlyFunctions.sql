931bed48c98eda808b46748d126892cff0fdc08d
Michael Wiencek <mwtuea@gmail.com>
2026-05-09 16:26:57 -0500

    Populate sql/DropStandaloneOnly*.sql files

---
+++ sql/DropStandaloneOnlyFunctions.sql
@@ -0,0 +1,3 @@
+\unset ON_ERROR_STOP
+
+DROP FUNCTION IF EXISTS sir.a_del_sir_pending_data();
---
+++ sql/DropStandaloneOnlyTriggers.sql
@@ -0,0 +1,3 @@
+\unset ON_ERROR_STOP
+
+DROP TRIGGER IF EXISTS cleanup_dbmirror2_pending_data_sir ON sir.pending_data;
